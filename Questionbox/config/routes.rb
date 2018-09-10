d768bf6315f0558d6ebb13e658d9af7206319eb0
lstravers <stravers222@gmail.com>
2018-09-10 17:13:13 -0400

    add application controller, modified routes, add user view

--- Questionbox/config/routes.rb
+++ Questionbox/config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :session, only: [:new, :create, :destroy]
+  resources :users
+
+  root 'questions#index'
+  resources :questions
+  resources :answers
+
+  # resources :posts do
+  #   member do
+  #     put "like" => "posts#vote"
+  #   end
+  # end
+
 end
