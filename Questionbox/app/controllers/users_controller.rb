5c644f935a4fffd84fcb459c9396460c1116d45d
Stephen Patterson <swpat23@gmail.com>
2018-09-11 17:07:39 -0400

    added mailer, still building

--- Questionbox/app/controllers/users_controller.rb
+++ Questionbox/app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   
   private
   def user_params
-    params.require(:user).permit(:username, :password, :photo)
+    params.require(:user).permit(:username, :password, :photo, :email)
   end
 
   def set_user
